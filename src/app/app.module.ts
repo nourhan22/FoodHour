151128dbbb59dd85cdb613adf3b731db1e4a5bad
nourhan22 <nourhan_adlo@yahoo.com>
2018-05-20 17:14:14 +0200

    'noura'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,11 @@ import { IngredientListingComponent } from './ingredients/ingredient-listing/ing
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HeaderComponent} from './shared/components/header/header.component';
-
 import {CuttingMethodsModule} from './cutting-methods/cutting-methods.module';
 import {CuttingMethodItemComponent} from './cutting-methods/cutting-method-item/cutting-method-item.component';
-
-
 import {ToolsModule} from './tools/tools.module';
 import {ToolItemComponent} from './tools/tool-item/tool-item.component';
 import { AppComponent } from './app.component';
-
-
-
-
 import {RouterModule} from '@angular/router';
 import { MicroOrganismItemComponent } from 'src/app/micro-organisms/micro-organism-item/micro-organism-item.component';
 import { from } from 'rxjs/internal/observable/from';
