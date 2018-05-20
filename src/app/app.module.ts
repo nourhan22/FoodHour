56cd8a2e346c44652eb8530bb936a752cd0900a3
nourhan22 <nourhan_adlo@yahoo.com>
2018-05-20 13:46:37 +0200

    share2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CuttingMethodsModule} from './cutting-methods/cutting-methods.module';
-
+import {CookingMethodsModule} from './cooking-methods/cooking-methods.module';
 import {CuttingMethodItemComponent} from './cutting-methods/cutting-method-item/cutting-method-item.component';
 import { CookingMethodItemComponent } from './cooking-methods/cooking-method-item/cooking-method-item.component';
 import { AppComponent } from './app.component';
