390020c7dde4d624591c36dd9fc52b901c11c31c
unknown <esraaezzat@gmail.com>
2018-05-20 13:36:44 +0200

    'deleteshared'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CuttingMethodsModule} from './cutting-methods/cutting-methods.module';
 import {CuttingMethodItemComponent} from './cutting-methods/cutting-method-item/cutting-method-item.component';
-
+import {MicroOrganismsModule} from './micro-organisms/micro-organisms.module'
 import { AppComponent } from './app.component';
+
+import { MicroOneItemComponent } from 'src/app/micro-organisms/micro-organism-listing/micro-one-item/micro-one-item.component';
+import { MicroOrganismListingComponent } from 'src/app/micro-organisms/micro-organism-listing/micro-organism-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CuttingMethodItemComponent
+    CuttingMethodItemComponent,
+    MicroOneItemComponent,
+    MicroOrganismListingComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
