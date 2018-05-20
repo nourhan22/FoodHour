478b14128e726e7beedc5914fe128846b6fa045f
Reem <reem.ahmed18295@gmail.com>
2018-05-20 11:53:25 +0200

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CuttingMethodsModule} from './cutting-methods/cutting-methods.module';
+import {CuttingMethodItemComponent} from './cutting-methods/cutting-method-item/cutting-method-item.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuttingMethodItemComponent
   ],
   imports: [
     BrowserModule
