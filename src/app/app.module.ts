28d957b023d336d0f2a48528e270d38c3c7a9502
sarashid <sarashid53@gmail.com>
2018-05-20 14:31:25 +0200

    ~toolitem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {CuttingMethodsModule} from './cutting-methods/cutting-methods.module';
 import {CuttingMethodItemComponent} from './cutting-methods/cutting-method-item/cutting-method-item.component';
-
+import {ToolsModule} from './tools/tools.module';
+import {ToolItemComponent} from'./tools/tool-item/tool-item.component';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs/internal/observable/from';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CuttingMethodItemComponent
+    CuttingMethodItemComponent,
+    ToolItemComponent
   ],
   imports: [
     BrowserModule
