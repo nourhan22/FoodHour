da01e453758639346693b61d342ea4304dd5a0a9
nourhan22 <nourhan_adlo@yahoo.com>
2018-05-22 12:50:07 +0200

    ggg

--- src/app/cutting-methods/cutting-methods.module.ts
+++ src/app/cutting-methods/cutting-methods.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CuttingMethodAddComponent } from './cutting-method-add/cutting-method-add.component';
 import { CuttingMethodItemComponent } from './cutting-method-item/cutting-method-item.component';
 import { CuttingMethodListingComponent } from './cutting-method-listing/cutting-method-listing.component';
-import { CuttingcomponentComponent } from './cuttingcomponent/cuttingcomponent.component';
+//import { CuttingcomponentComponent } from './cuttingcomponent/cuttingcomponent.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [CuttingMethodAddComponent, CuttingMethodItemComponent, CuttingMethodListingComponent,
-    CuttingcomponentComponent
+    //CuttingcomponentComponent
 ]
 })
 export class CuttingMethodsModule { }
