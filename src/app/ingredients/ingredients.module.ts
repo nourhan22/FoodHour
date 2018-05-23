b3d6b09c9bb53d8d5b611bb7d86a22426b760e27
nayera <nayerasaad27@gmail.com>
2018-05-23 21:40:09 +0200

    firstcommit

--- src/app/ingredients/ingredients.module.ts
+++ src/app/ingredients/ingredients.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IngredientAddComponent } from './ingredient-add/ingredient-add.component';
 import { IngredientItemComponent } from './ingredient-item/ingredient-item.component';
 import { IngredientListingComponent } from './ingredient-listing/ingredient-listing.component';
-import { IngredientNutiritionsComponent } from './ingredient-nutiritions/ingredient-nutiritions.component';
+
 import { IngredientDetailsComponent } from './ingredient-details/ingredient-details.component';
 
 @NgModule({
@@ -11,7 +11,7 @@ import { IngredientDetailsComponent } from './ingredient-details/ingredient-deta
     CommonModule
   ],
   declarations: [IngredientAddComponent, IngredientItemComponent, IngredientListingComponent,
-    IngredientNutiritionsComponent,
+
     IngredientDetailsComponent
 ]
 })
