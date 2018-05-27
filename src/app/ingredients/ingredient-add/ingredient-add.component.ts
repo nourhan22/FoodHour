278105c5488c40ebe503178db080eec05b460be4
Raghda Gamal <raghdagamal77@gmail.com>
2018-05-28 01:12:31 +0200

    DEVELOP: Fix Changes

--- src/app/ingredients/ingredient-add/ingredient-add.component.ts
+++ src/app/ingredients/ingredient-add/ingredient-add.component.ts
@@ -5,11 +5,6 @@ import { INutiritionType } from '../../shared/models/interfaces/INutiritionType'
 import { IngredientService } from '../../shared/services/ingredient.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a134678c7297cef6a6da252bd5fd37664195dacc
 
 @Component({
   selector: 'app-ingredient-add',
@@ -44,12 +39,7 @@ export class IngredientAddComponent implements OnInit {
       if(this.newIngredient!=null && myform.valid)
       {
         this.newIngredient.id = this.ingredientService.lengthOfIngredients+1;
-<<<<<<< HEAD
-        this.newIngredient.img = '../assets/images/default.png';
-         
-=======
         this.newIngredient.img = '../assets/images/default.png'
->>>>>>> a134678c7297cef6a6da252bd5fd37664195dacc
         this.ingredientService.AddNewIngredient(this.newIngredient);   
         this.router.navigate(['IngredientListing']) ;       
       }
@@ -59,11 +49,6 @@ export class IngredientAddComponent implements OnInit {
     {
       this.router.navigate(['IngredientListing']) ;      
     }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a134678c7297cef6a6da252bd5fd37664195dacc
   //---------------------------------------------------------------
 //   $(document).ready(function() {
 //     $("#addType").click(function () {
@@ -74,7 +59,6 @@ export class IngredientAddComponent implements OnInit {
 //         else {
 //             x.style.display = "block";
 //         }
-<<<<<<< HEAD
 
 
 //         //$("#myForm")[0].reset();
@@ -92,25 +76,6 @@ export class IngredientAddComponent implements OnInit {
 //         //$("table:first tr td:last-child").addClass('Delete');
 //         $("#mytable").append('<tr><td class="counterCell"></td><td>' + v1 + '</td><td>' + v2 + '</td><td>' + v3 +'</td><td><button class="Delete" onclick="myFun(this)"><i class="fa fa-times"></i></button></td></tr>');
 
-=======
-
-
-//         //$("#myForm")[0].reset();
-
-
-//         //////////////////
-//         //var v0='<td class="counterCell"></td>';
-//         var v1 = $('#dropDownType option:selected').val();
-//         var v2 = $('#NutiritionType').val();
-//         var v3 = $('#Percentage').val();
-
-
-
-//         var tds = v1 + v2+v3;
-//         //$("table:first tr td:last-child").addClass('Delete');
-//         $("#mytable").append('<tr><td class="counterCell"></td><td>' + v1 + '</td><td>' + v2 + '</td><td>' + v3 +'</td><td><button class="Delete" onclick="myFun(this)"><i class="fa fa-times"></i></button></td></tr>');
-
->>>>>>> a134678c7297cef6a6da252bd5fd37664195dacc
 //     });
 
 }
