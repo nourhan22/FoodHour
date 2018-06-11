1af6e0fe8f47682bed78bc926c49e9f6c6e3f2d0
Reem <reem.ahmed18295@gmail.com>
2018-06-11 14:27:05 +0200

    postservices?

--- src/app/community/community.module.ts
+++ src/app/community/community.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
+import {DetailsComponent} from './details/details.component';
 
 import { PostListingComponent } from './post-listing/post-listing.component';
 import { PostContentComponent } from './post-content/post-content.component';
