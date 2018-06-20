e90cd18dc9f41d3ef4b90b32d15d5e59092346dd
sarashid <sarashid53@gmail.com>
2018-06-20 11:41:49 +0200

    commit to esraa

--- src/app/recipe/recipe-details/miseEnPlace/miseEnPlace.component.ts
+++ src/app/recipe/recipe-details/miseEnPlace/miseEnPlace.component.ts
@@ -15,13 +15,11 @@ export class MiseEnPlaceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    debugger;
-    if(this.RecipeItem.ingredients.length!=0)
+    if(this.RecipeItem.steps.length==0)
     {
       this.RecipeItem.steps = this.RecipeItem.ingredients.map(function(r){return {title:r.ingredient.name,
         description:r.procedure , imageUrl:r.image}})
     }
-  
     this.item=this.RecipeItem.steps[this.currentIndex];
     this.end=this.RecipeItem.steps.length-1;
   }
@@ -44,5 +42,4 @@ export class MiseEnPlaceComponent implements OnInit {
     this.item=this.RecipeItem.steps[this.currentIndex];
 
   }
-
 }
